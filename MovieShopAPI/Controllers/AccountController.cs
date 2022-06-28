d177ed2f6406d061c5f800d729275049d0c27420
Brandon Girard <branmic11@gmail.com>
2022-06-28 16:14:01 -0400

    Added jwt token creation and login API

--- MovieShopAPI/Controllers/AccountController.cs
+++ MovieShopAPI/Controllers/AccountController.cs
@@ -2,6 +2,10 @@
 using ApplicationCore.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MovieShopAPI.Controllers
 {
@@ -10,10 +14,12 @@ namespace MovieShopAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
+        private readonly IConfiguration _configuration;
 
-        public AccountController(IAccountService accountService)
+        public AccountController(IAccountService accountService, IConfiguration configuration)
         {
             _accountService = accountService;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -25,7 +31,58 @@ namespace MovieShopAPI.Controllers
             return Ok(user);
         }
 
-        //public async Task<IActionResult> Login()
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginModel model)
+        {
+            var user = await _accountService.ValidateUser(model.Email, model.Password);
+
+            if (user != null) {
+                var jwtToken = CreateJwtToken(user);
+                return Ok( new { token = jwtToken});
+            }
+                
+
+            return Unauthorized(new { errorMessage = "Please check email and password" });
+
+        }
+
+        private string CreateJwtToken(UserModel user)
+        {
+            var claims = new List<Claim>
+            {
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
+                    new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+                    new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                    new Claim("Country", "USA"),
+                    new Claim("Language", "english")
+            };
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["secretKey"]));
+
+            var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenExpiration = DateTime.UtcNow.AddHours(2);
+
+            var tokenDetails = new SecurityTokenDescriptor
+            {
+                Subject = identityClaims,
+                Expires = tokenExpiration,
+                SigningCredentials = credentials,
+                Issuer = "MovieShop, Inc",
+                Audience = "MovieShop Clients"
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var encodedJwt = tokenHandler.CreateToken(tokenDetails);
+
+            return tokenHandler.WriteToken(encodedJwt);
+        }
 
         //public async Task<IActionResult> CheckEmail()
     }
